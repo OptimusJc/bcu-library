2e916c64db1675a99dd466beb4af5aaa0f52a01f
OptimusJc <otienoj.cruz@outlook.com>
2022-05-05 17:01:23 +0300

    style: Add styles for ipad footer experience

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,77 +6,82 @@ import styles from "../header/Navigation.module.css";
 import { FaFacebook, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
-  return (
-    <footer className={["container", styles.backgroundColor].join(" ")}>
-      <div className="footer-logo">
-        <img src={footer_logo} alt="footer logo" />
-      </div>
+    return (
+        <footer className={["container", styles.backgroundColor].join(" ")}>
+            <div className={["footer"]}>
+                <div className="footer-logo">
+                    <img src={footer_logo} alt="footer logo" />
+                </div>
 
-      <div>
-        <ul>
-          <li>About</li>
-          <li>
-            <Link to="">About the Library</Link>
-          </li>
-          <li>
-            <Link to="">Contact Us</Link>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <ul>
-          <li>Resources</li>
-          <li>
-            <Link to="">Ebooks</Link>
-          </li>
-          <li>
-            <Link to="">Collections</Link>
-          </li>
-          <li>
-            <Link to="">Messages</Link>
-          </li>
-          <li>
-            <Link to="">Songs</Link>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <ul>
-          <li>Help</li>
-          <li>
-            <Link to="">FAQ's</Link>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <ul>
-          <li>Support</li>
-          <li>
-            <Link to="">Volunteer</Link>
-          </li>
-          <li>
-            <Link to="">Donate</Link>
-          </li>
-        </ul>
-      </div>
+                <div>
+                    <ul>
+                        <li>About</li>
+                        <li>
+                            <Link to="">About the Library</Link>
+                        </li>
+                        <li>
+                            <Link to="">Contact Us</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li>Resources</li>
+                        <li>
+                            <Link to="">Ebooks</Link>
+                        </li>
+                        <li>
+                            <Link to="">Collections</Link>
+                        </li>
+                        <li>
+                            <Link to="">Messages</Link>
+                        </li>
+                        <li>
+                            <Link to="">Songs</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li>Help</li>
+                        <li>
+                            <Link to="">FAQ's</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li>Support</li>
+                        <li>
+                            <Link to="">Volunteer</Link>
+                        </li>
+                        <li>
+                            <Link to="">Donate</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
 
-      <div className="footer-bottom">
-        <ul className="footer-icons">
-          <li>
-            <Link to="" className="facebook">
-              <FaFacebook />
-            </Link>
-          </li>
-          <li>
-            <Link to="" className="youtube">
-              <FaYoutube />
-            </Link>
-          </li>
-        </ul>
-        <p>&#169;2022. Body of Christ United Global International Ministries</p>
-      </div>
-    </footer>
-  );
+            <div className="footer-bottom">
+                <ul className="footer-icons">
+                    <li>
+                        <Link to="" className="facebook">
+                            <FaFacebook />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="" className="youtube">
+                            <FaYoutube />
+                        </Link>
+                    </li>
+                </ul>
+                <p>
+                    &#169;2022. Body of Christ United Global International
+                    Ministries
+                </p>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
