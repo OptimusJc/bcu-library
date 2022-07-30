5bb5c82a7d2ca3ce08aa7121d45b9b737505454b
OptimusJc <optimus.prime.jc@gmail.com>
2022-07-30 13:14:13 +0300

    feat: Add new background image

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -7,15 +7,17 @@ import "../buttons/button.modules.css";
 // import hero_image from "../../assets/images/hero_image.jpg";
 
 const Hero = () => {
-    return (
-        <div className="container_fluid">
-            <section className="hero">
-                <p className="display">Search the Library Catalog</p>
-                <Button title="search" classes="btn btn-search" />
-                <Input type="text" placeholder="Search" id="search" />
-            </section>
-        </div>
-    );
+	return (
+		<div className="container_fluid hero">
+			<section className="container">
+				<div>
+					<p className="display">Search the Library Catalog</p>
+					<Button title="search" classes="btn btn-search" />
+					<Input type="text" placeholder="Search" id="search" />
+				</div>
+			</section>
+		</div>
+	);
 };
 
 export default Hero;
