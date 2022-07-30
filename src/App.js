b26edbbdd611e09cc18e5d80615a5c74e2f87c7e
OptimusJc <optimus.prime.jc@gmail.com>
2022-07-30 15:12:13 +0300

    feat: Add the background assets

--- src/App.js
+++ src/App.js
@@ -13,14 +13,14 @@ import Footer from "./components/footer/Footer";
 import "./index.css";
 
 function App() {
-    return (
-        <div>
-            <Navigation />
-            {/* enables the rendering of the site pages */}
-            <Outlet />
-            <Footer />
-        </div>
-    );
+	return (
+		<div>
+			<Navigation />
+			{/* enables the rendering of the site pages */}
+			<Outlet />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
