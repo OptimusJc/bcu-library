38201357532ff07344df0671578ca50894558b84
OptimusJc <optimus.prime.jc@gmail.com>
2022-08-18 16:41:05 +0300

    feat: Add the personality of the week section

--- src/components/personality_section/Personality.js
+++ src/components/personality_section/Personality.js
@@ -1,23 +1,27 @@
 import { Link } from "react-router-dom";
 import "./Personality.modules.css";
+import bishopImage from "../../assets/images/personalities/paster-td-jakes.jpg";
 
 const Personality = () => {
 	return (
 		<section className="container">
 			<div className="personality-section">
 				<div className="person_image">
-					<img src="https://via.placeholder.com/400x400" alt="" />
+					<img
+						src={
+							bishopImage ? bishopImage : "https://via.placeholder.com/400x400"
+						}
+						alt=""
+					/>
 				</div>
 				<div className="person_bio animate__animated animate__bounce">
 					<h3>personality of the week</h3>
-					<h4>dr. flinstones</h4>
+					<h4>Bishop T.D Jakes</h4>
 					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-						aliquip ex ea commodo consequat. Duis aute irure dolor in
-						reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-						pariatur
+						Bishop T.D. Jakes, one of the world’s most revered masterminds,
+						leverages his pioneering vision and instinct to serve others in
+						areas extending beyond the church. In order to help lead people to
+						their destiny, you have to meet people where they are in life.
 					</p>
 
 					{/* <Button title="Read More" classes="read-more" /> */}
--- src/components/personality_section/PersonalityDetails.jsx
+++ src/components/personality_section/PersonalityDetails.jsx
@@ -1,4 +1,5 @@
 import "./PersonalityDetails.modules.css";
+import bishopImage from "../../assets/images/personalities/bishop_and_firstlady.png";
 
 const PersonalityDetails = (url) => {
 	return (
@@ -6,23 +7,197 @@ const PersonalityDetails = (url) => {
 			<div className="row">
 				<div className="col personality">
 					<h1>Personality of the week</h1>
-					<h3>Personality Name</h3>
-					<h5>Church</h5>
+					<h3>Bishop T.D Jakes</h3>
+					<h6>THE POTTER'S HOUSE</h6>
 
 					<div className="personality-body">
 						<div className="personality-image">
-							<img src="https://via.placeholder.com/350x320" alt="" />
+							<img
+								src={
+									bishopImage
+										? bishopImage
+										: "https://via.placeholder.com/350x320"
+								}
+								alt=""
+							/>
 						</div>
 
 						<div className="personality-text">
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-								eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-								enim ad minim veniam, quis nostrud exercitation ullamco laboris
-								nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-								in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-								nulla pariatur
-							</p>
+							<section>
+								<p>
+									Bishop T.D. Jakes, one of the world’s most revered
+									masterminds, leverages his pioneering vision and instinct to
+									serve others in areas extending beyond the church. In order to
+									help lead people to their destiny, you have to meet people
+									where they are in life. It is with this earnest approach that
+									Bishop Jakes has been able to reach millions of people from
+									all socioeconomic backgrounds, races, nationalities, and
+									creeds. Digital media, film, and television, among others,
+									have been instrumental in helping Bishop Jakes meet the
+									disparate needs of countless individuals.
+								</p>
+								<p>
+									In September 2016, T.D. Jakes launched the{" "}
+									<i>TD Jakes Show</i>, an intellectually invigorating, yet
+									practical daytime talk show, where Bishop helps his guests
+									lead empowering lives through candid conversation. As a
+									visionary leader, T.D. Jakes encourages you to think
+									differently – and progressively – so that you can create a
+									more meaningful life experience that is truly your own. The
+									adage, “Give a man a fish and you feed him for a day; teach a
+									man to fish and you feed him for a lifetime” is the bedrock of
+									Bishop’s visionary leadership.
+								</p>
+							</section>
+							<section>
+								<h5>Faithful Pastor</h5>
+								<p>
+									For more than 40 years, Bishop T.D. Jakes has helped millions
+									of people realize their purpose through his dynamic ministry.
+									Recognized as “America’s Best Preacher” by Time Magazine, as
+									well as “One of the Nation’s Most Influential & Mesmerizing
+									Preachers” by <i>The New York Times</i>, Bishop Jakes remains
+									a charismatic, yet humble man. In 1996, with minimal
+									resources, T.D. Jakes founded{" "}
+									<strong>The Potter’s House (TPH)</strong>, a
+									non-denominational, multicultural church and global
+									humanitarian organization, in Dallas, Texas. The church has
+									since expanded to include more than 30,000 members, with more
+									than 50 diverse ministries. While TPH, located in southern
+									Dallas, is the designated home to numerous members, Bishop
+									Jakes has fulfilled the needs of those residing in the
+									northern and western regions of the Dallas-Fort Worth
+									metroplex, as well as out-of-state. TPH campuses are also
+									present in Frisco and Fort Worth, Texas, as well as Denver,
+									Colorado.
+								</p>
+								<p>
+									Bishop Jakes faithfully serves his local and global
+									communities to embolden those who are marginalized, from
+									Dallas, Texas to South Africa to Sydney, Australia. “The
+									Potter’s House charter is to extend a hand of help to the
+									needy, a heart of compassion to the hurting, and a message of
+									empowerment to the disenfranchised.” To this end, Bishop Jakes
+									provides a welcoming church environment to all. Unequivocally,
+									the diversity displayed throughout TPH is what makes it a
+									truly progressive church, serving people of every
+									socioeconomic status, age, nationality, and race. Each year
+									Bishop Jakes helps underserved communities through TPH
+									ministries, including homeless outreach, GED/literacy
+									programs, and AIDS outreach. Bishop Jakes also empowers
+									communities through distinct national ministries, including
+									Global Partner System (GPS), MegaCARE, and Texas Offenders
+									Reentry Initiative (T.O.R.I.). With a steadfast love of
+									people, TPH is consistently ranked amongst the nation’s most
+									influential churches each year.
+								</p>
+							</section>
+
+							<section>
+								<h5>Charismatic Influencer</h5>
+								<blockquote>
+									“Others can inspire you, but ultimately the only thing that
+									empowers you is what lies within you and learning how to
+									better utilize what you’ve been given.” -T.D Jakes,
+									<cite>Instinct: The Power to Unleash Your Inborn Drive.</cite>
+								</blockquote>
+								<p>
+									A proclivity to disrupt the status quo, Bishop T.D. Jakes is
+									one of the most coveted faith-based influencers in business,
+									education, film, and entertainment. T.D. has advised the last
+									three presidents; in 2009, he was a featured speaker at the
+									inauguration of Barack Obama; and in 2011, he gave remarks at
+									a White House Easter Prayer Breakfast. From Savoy Magazine
+									recognizing Bishop Jakes as one of the “Top 100 Powers That
+									Be” to <strike>Ebony Magazine</strike> honoring him on their
+									“Power 100 List,” Bishop Jakes continues to show the world how
+									to lead with purpose. While T.D. Jakes possesses myriad
+									leadership qualities, his ability to leverage his linguistic
+									prowess, strategic insights, and servant spirit is
+									unparalleled. Some of the most successful business executives
+									and leaders travel from all over the world seeking Bishop’s
+									counsel. T.D. Jakes has offered his insights at diverse
+									forums, including The Aspen Institute, The Willow Creek
+									Leadership Forum, and The SMU Tate Lecture Series, among
+									others. His efforts have helped some of the world’s most
+									illustrious leaders elevate their brands to the next level.
+								</p>
+								<p>
+									Bishop Jakes also connects communities from across the globe
+									through various multicultural conferences. For more than a
+									decade, <strike>MegaFest</strike> continues to receive
+									international recognition as the nation’s largest faith and
+									family festival. This phenomenal conference draws upwards of
+									100,000 attendees from as far as Singapore, China. From Woman
+									Thou Art Loosed, ManPower, MegaKidz, to Fire Code Youth, this
+									conference will entertain and educate the entire family.
+									Additionally, Bishop Jakes offers entrepreneurs, pastors, and
+									leaders an opportunity to hone their leadership skills through
+									the
+									<strike>
+										International Pastors and Leadership Conference
+									</strike>
+									. Bishop Jakes encourages you to attend these transformative
+									conferences and events, as the wealth of knowledge that he
+									offers his audiences is invaluable.
+								</p>
+							</section>
+
+							<section>
+								<h5>Best-selling Author & Award-winning Filmmaker</h5>
+								<p>
+									Named “America’s Treasure” by Oprah Winfrey, T.D. Jakes is a
+									master communicator for all ages. His prose flows like honey
+									from a soft Melianthus comosus. While many of his followers
+									have familiarized themselves with Bishop’s sermons, few know
+									that he also relishes making people laugh, especially when
+									trying to overcome life’s most daunting circumstances. The
+									adage, “Sometimes you have to laugh to keep from crying” helps
+									to keep one’s heart and soul healthy. While not every creative
+									work that Bishop publishes displays overt hilarity, Bishop
+									Jakes delicately weaves in a tone and style that helps the
+									reader or movie patron feel a sense of peace and healing. T.D.
+									Jakes offers a collection of award-winning and{" "}
+									<i>New York Times</i>
+									best-selling books, including{" "}
+									<span>Destiny: Step Into Your Purpose</span>
+									and{" "}
+									<span>Instinct: The Power to Unleash Your Inborn Drive</span>.
+									In the entertainment space, Bishop Jakes has numerous film
+									credits to his name, including the award-winning romantic
+									comedy, <span>Jumping the Broom</span>, among others.
+								</p>
+							</section>
+
+							<section>
+								<h5>Devoted Husband</h5>
+								<p>
+									As a devoted husband to First Lady Serita Jakes, Bishop Jakes
+									eagerly supports his beautiful wife of more than 35 years.
+									Serita Jakes, a best friend, a mother, a grandmother, and a
+									loving wife, is Bishop’s chief advocate as well. Serita and
+									T.D. have managed every business opportunity, as well as
+									personal endeavors with their unwavering faith in God. The
+									power couple has successfully raised five beautiful grown
+									children, including Sarah Jakes, Cora Jakes-Coleman, Thomas
+									Jakes, Jr., Jermaine Jakes, and Jamar Jakes.
+								</p>
+								<p>
+									The secret to their longevity belies “Hollywood marriage
+									advice”. Through the fortunate and not-so-fortunate times,
+									Serita and T.D. support each other every step of the way. He
+									encourages Serita to pursue her passions in the ministry, as
+									well as her business endeavors. To this end, Serita serves as
+									executive director for The Potter’s House Women’s Ministries,
+									among other roles, serving the domestic violence community.
+									With an adoration of design, Serita Jakes recently launched
+									Serita Jakes Home, a thoughtfully-designed collection of
+									candles and home décor accent pieces. Whether enjoying a long,
+									relaxing walk along the beach or launching a business, T.D.
+									and Serita Jakes support each other with their faith in God,
+									and in each other.
+								</p>
+							</section>
 						</div>
 					</div>
 				</div>
