d71be1a60a8f7d3fc86d2c00664a316e8b011f1d
OptimusJc <optimus.prime.jc@gmail.com>
2022-08-25 16:55:16 +0300

    core: Add the mobile version of the supportus page

---
+++ src/pages/support/SupportUs.jsx
@@ -0,0 +1,32 @@
+import "./SupportUs.modules.css";
+
+function SupportUs() {
+	return (
+		<div>
+			<div className="supportus_hero_section">
+				<div className="container">
+					<div className="row">
+						<div className="col">
+							<div className="supportus_hero">
+								<div className="supportus_hero_body">
+									<h6 className="h6 supportus_hero_subtitle">
+										BCU Partnership Program
+									</h6>
+									<p className="supportus_hero_title">
+										"BCU... brethren can unite to fulfill the greatest
+										commandment; to love by action.{" "}
+										<span>
+											Partner with BCU, Body of Christ United Global movement"
+										</span>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default SupportUs;
