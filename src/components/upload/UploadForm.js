b075f803e9b46d3fda92b3a5f9518536be1389e0
OptimusJc <optimus.prime.jc@gmail.com>
2022-12-27 02:27:19 +0300

    Revert "feat: Add new admin upload panel"
    This reverts commit eb32d04734a7726bdd6bd0dd7ef3de557d331962.

--- src/components/upload/UploadForm.js
+++ src/components/upload/UploadForm.js
@@ -1,48 +1,118 @@
 import { useState } from "react";
 import Progress from "../progress/Progress";
-import Form from "./form/Form";
+import UploadInput from "./UploadInput/UploadInput.jsx";
+import useFirestore from "../hooks/useFirestore";
 
 const UploadForm = () => {
-	const [file, setFile] = useState(null);
+	const [files, setFiles] = useState(null);
 	const [error, setError] = useState(null);
-	const [selected, setSelected] = useState(null);
-	const [description, setDescription] = useState("");
-	const [collection, setCollection] = useState("");
+
+	// // *Start
+	// const onChangeHandler = (e) => {
+	// 	// * Get the files array
+	// 	const selected = [...e.target.files];
+
+	// 	// * file types
+	// 	// const types = ["audio/mpeg", "audio/mp3", "video/mp4", "video/mpeg"];
+	// 	const types = ["application/pdf", "application/epub+zip"];
+
+	// 	if (selected) {
+	// 		selected.map((selectedFile) => {
+	// 			// fileTypes.includes(selectedFile.type);
+	// 			if (fileTypes.includes(selectedFile.type)) {
+	// 				setError("works ok!");
+	// 				return setFiles(selectedFile);
+	// 			} else {
+	// 				setFiles(null);
+	// 				setError("Please select a pdf or epub file");
+	// 				// setError("Please select an audio or video file");
+	// 			}
+	// 		});
+	// 	} else {
+	// 		setError("No files selected!!");
+	// 	}
+	// };
+	// // * [end]
+
+	// *Start
+	const onChangeHandler = (e) => {
+		// * Get the files array
+		const selected = [...e.target.files];
+
+		// * file types
+		// const types = ["audio/mpeg", "audio/mp3", "video/mp4", "video/mpeg"];
+		const types = ["application/pdf", "application/epub+zip"];
+
+		if (selected) {
+			selected.map((selectedFile) => {
+				return types.includes(selectedFile.type)
+					? (setFiles(selected), setError(""))
+					: setError("Please select pdf/epub files");
+			});
+		} else {
+			setError("Can not upload nothing😃❗");
+		}
+	};
+	// * [end]
+
+	const podcasts = useFirestore("ebooks/children_ebook/fiction");
+	console.log(podcasts.length);
 
 	return (
 		<div className="container">
 			<div className="row">
-				<div className="col">
-					<Form
-						selected={selected}
-						setSelected={setSelected}
-						error={error}
-						setError={setError}
-						collection={collection}
-						setCollection={setCollection}
-						file={file}
-						setFile={setFile}
-						description={description}
-						setDescription={setDescription}
-					/>
+				<div className="col" style={{ height: "500px", marginTop: "2rem" }}>
+					<form>
+						<h3>Upload Section</h3>
+						<p className="lead">Choose a collection to upload to</p>
 
-					{/* *output feedback */}
-					<div className="output">
-						{error && <div className="error">{error}</div>}
-
-						{file && (
-							<Progress
-								file={file}
-								setFile={setFile}
-								description={description}
-								setDescription={setDescription}
-								collectionName={collection}
-								setCollection={setCollection}
-								selected={selected}
-								setSelected={setSelected}
+						{/* <div className="mb-3">
+							<input
+								className="form-check-input"
+								type="radio"
+								name="flexRadioDefault"
+								id="ebookCollection"
+								value="ebooks"
 							/>
-						)}
-					</div>
+							<label className="form-check-label" htmlFor="ebookCollection">
+								Ebooks
+							</label>
+						</div>
+						<div className="mb-3">
+							<input
+								className="form-check-input"
+								type="radio"
+								name="flexRadioDefault"
+								id="podcastCollection"
+								value="podcasts"
+							/>
+							<label className="form-check-label" htmlFor="podcastCollection">
+								Podcasts
+							</label>
+						</div> */}
+
+						<UploadInput onChange={onChangeHandler} />
+
+						{/* *output feedback */}
+						<div className="output">
+							{error && <div className="error">{error}</div>}
+							{files && (
+								<div>
+									{files.map((file, index) => {
+										return <p key={index}>{file.name}</p>;
+									})}
+								</div>
+							)}
+
+							{files && <Progress files={files} setFiles={setFiles} />}
+						</div>
+						<input
+							type="submit"
+							value="upload"
+							className="btn btn-primary mt-4"
+						/>
+					</form>
+					<p>Number of files uploaded: {podcasts.length}</p>
 				</div>
 			</div>
 		</div>
